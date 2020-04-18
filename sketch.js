bebd3714855b82a27e0f36173f27b9a9e2c04f9c
Jon Martin <jonathanmartinnyc@gmail.com>
2020-04-18 16:06:04 -0400

    major update - thump sound, smaller hearts
    using p5.oscillator to make thumping sound
    *need to find a way to make it less crackling
    constructor for hearts now has a blocksize parameter

--- sketch.js
+++ sketch.js
@@ -9,17 +9,27 @@ var heart1;
 var heart2;
 var heartArr = [];
 var countDown1;
+var screenWidth = 1000;
+var screenHeight = 600;
+
+var wave;
+
+
 
 function setup() {
-  createCanvas(1200, 800);
+  createCanvas(screenWidth, screenHeight);
   for (var i = 0; i < 8; i++) {
-    heartArr.push(new heart(random(1200), random(800), 9));
+    heartArr.push(new heart(random(screenWidth), random(screenHeight), 10,9));
   }
   //heart1 = new heart(x, y, 9);
-  heart2 = new heart(400, 400, 9);
+  heart2 = new heart(400, 400, 10,9);
   heart2.velocity.setMag(0);
+  heart2.player = true;
 
-  CountDown1 = new CountDown(100, 100);
+  countDown1 = new CountDown(100, 100);
+    wave = new p5.Oscillator;
+    wave.setType('sine');
+
 
 }
 
@@ -27,7 +37,7 @@ function draw() {
   background(0);
 
 
-  for (var i = 0; i < heartArr.length; i++) {
+  for (let i = 0; i < heartArr.length; i++) {
     heartArr[i].timer();
     heartArr[i].update();
     heartArr[i].checkAlive();
@@ -36,24 +46,24 @@ function draw() {
   heart2.update();
   heart2.checkAlive();
 
-  CountDown1.update();
+  countDown1.update();
 
   if (heartArr.length === 0 || heart2.row < 0) {
-    CountDown1.color = 'red';
-    CountDown1.update();
+      countDown1.color = 'red';
+      countDown1.update();
     noLoop();
 
   }
 
 
   //check for collision
-  for (var i = 0; i < heartArr.length; i++) {
-    if (dist(heartArr[i].location.x, heartArr[i].location.y - 80, heart2.location.x, heart2.location.y - 80) < 200) {
+  for (let i = 0; i < heartArr.length; i++) {
+    if (dist(heartArr[i].location.x, heartArr[i].location.y - heartArr[i].blockSize*4, heart2.location.x, heart2.location.y - heart2.blockSize*4) < heart2.blockSize*10) {
       //temp test collision
       //heartArr[i].xVel *= -1;
       //heartArr[i].yVel *= -1;
       heartArr[i].velocity.add(heart2.velocity);
-      
+
       heartArr[i].location.x += heart2.velocity.x*2;
       heartArr[i].location.y += heart2.velocity.y*2;
       heartArr[i].born = millis();
@@ -61,7 +71,7 @@ function draw() {
     }
   }
 
-  for (var i = heartArr.length - 1; i >= 0; i--) {
+  for (let i = heartArr.length - 1; i >= 0; i--) {
     //console.log(heartArr);
     if (heartArr[i].alive === false) {
       heartArr.splice(i, 1);
@@ -85,6 +95,11 @@ function draw() {
   if (keyIsPressed && keyCode === DOWN_ARROW) {
     heart2.velocity.y += 1;
   }
+
+  //if(keyIsPressed){
+  //    wave.start();
+  //    wave.amp(0.1);
+  //}
 
 
 
@@ -122,9 +137,11 @@ class CountDown {
 
 class heart {
 
-  constructor(x, y, row) {
+  constructor(x, y, blockSize,row) {
     this.x = x;
     this.y = y;
+    this.blockSize = blockSize;
+    this.startSize = blockSize;
     this.row = row;
     this.born = millis();
     this.alive = true;
@@ -132,6 +149,7 @@ class heart {
     this.yVel = random(-2, 2);
     this.location = createVector(x, y);
     this.velocity = createVector(random(-2, 2), random(-2, 2));
+    this.player = false;
   }
 
   timer() {
@@ -147,27 +165,53 @@ class heart {
 
   }
 
+
   update() {
+
+      if(this.player === true){
+          if(frameCount%50===0){
+              wave.start();
+              wave.amp(0.1);
+              wave.freq(200);
+              //wave.stop();
+              this.startTime = frameCount;
+              this.blockSize = this.startSize*1.2;
+          }
+          else{
+              //wave.stop();
+              this.blockSize = this.startSize;
+          }
+
+          if(frameCount - this.startTime === 10){
+              wave.start();
+              wave.amp(0.1);
+              wave.freq(200);
+              //wave.stop();
+              this.blockSize = this.startSize*1.2;
+          }
+
+      }
+
     //this.x += this.xVel;
     //this.y += this.yVel;
-    this.velocity.limit(10);
+    this.velocity.limit(5);
     this.location.add(this.velocity);
 
-    if (this.location.x-80 < 0) {
-      //this.location.x = 80;
-      this.location.x += 1600;
+    if (this.location.x-this.blockSize*4 < 0) {
+      //this.location.x = this.blockSize*4;
+      this.location.x += screenWidth;
     }
-    if (this.location.x-80 > 1200) {
+    if (this.location.x-this.blockSize*4 > screenWidth) {
       //this.location.x = 1040;
-      this.location.x -= 1600;
+      this.location.x -= screenWidth;
     }
-    if (this.location.y-160 < 0) {
-       //this.location.y = 80;
-      this.location.y += 800;
+    if (this.location.y-this.blockSize*8 < 0) {
+       //this.location.y = this.blockSize*4;
+      this.location.y += screenHeight;
     }
-    if (this.location.y > 800) {
+    if (this.location.y > screenHeight) {
       //this.location.y = 640;
-     this.location.y -= 800;
+     this.location.y -= screenHeight;
     }
 
     this.display();
@@ -183,55 +227,55 @@ display() {
     } else {
       fill(255);
     }
-    rect(this.location.x, this.location.y, 20, 20);
+    rect(this.location.x, this.location.y, this.blockSize, this.blockSize);
     //row2
     if (2 <= this.row) {
       fill(255, 0, 0);
     } else {
       fill(255);
     }
-    rect(this.location.x - 20, this.location.y - 20, 20, 20);
-    rect(this.location.x, this.location.y - 20, 20, 20);
-    rect(this.location.x + 20, this.location.y - 20, 20, 20);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize, this.blockSize, this.blockSize);
     //row3
     if (3 <= this.row) {
       fill(255, 0, 0);
     } else {
       fill(255);
     }
-    rect(this.location.x - 40, this.location.y - 40, 20, 20);
-    rect(this.location.x - 20, this.location.y - 40, 20, 20);
-    rect(this.location.x, this.location.y - 40, 20, 20);
-    rect(this.location.x + 20, this.location.y - 40, 20, 20);
-    rect(this.location.x + 40, this.location.y - 40, 20, 20);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*2, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*2, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize*2, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*2, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*2, this.blockSize, this.blockSize);
     //row4
     if (4 <= this.row) {
       fill(255, 0, 0);
     } else {
       fill(255);
     }
-    rect(this.location.x - 60, this.location.y - 60, 20, 20);
-    rect(this.location.x - 40, this.location.y - 60, 20, 20);
-    rect(this.location.x - 20, this.location.y - 60, 20, 20);
-    rect(this.location.x, this.location.y - 60, 20, 20);
-    rect(this.location.x + 20, this.location.y - 60, 20, 20);
-    rect(this.location.x + 40, this.location.y - 60, 20, 20);
-    rect(this.location.x + 60, this.location.y - 60, 20, 20);
+    rect(this.location.x - this.blockSize*3, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*3, this.location.y - this.blockSize*3, this.blockSize, this.blockSize);
     //row5
     if (5 <= this.row) {
       fill(255, 0, 0);
     } else {
       fill(255);
     }
-    rect(this.location.x - 80, this.location.y - 80, 20, 20);
-    rect(this.location.x - 60, this.location.y - 80, 20, 20);
-    rect(this.location.x - 40, this.location.y - 80, 20, 20);
-    rect(this.location.x - 20, this.location.y - 80, 20, 20);
-    rect(this.location.x, this.location.y - 80, 20, 20);
-    rect(this.location.x + 20, this.location.y - 80, 20, 20);
-    rect(this.location.x + 40, this.location.y - 80, 20, 20);
-    rect(this.location.x + 60, this.location.y - 80, 20, 20);
-    rect(this.location.x + 80, this.location.y - 80, 20, 20);
+    rect(this.location.x - this.blockSize*4, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*3, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*3, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*4, this.location.y - this.blockSize*4, this.blockSize, this.blockSize);
 
     //row6
     if (6 <= this.row) {
@@ -239,15 +283,15 @@ display() {
     } else {
       fill(255);
     }
-    rect(this.location.x - 80, this.location.y - 100, 20, 20);
-    rect(this.location.x - 60, this.location.y - 100, 20, 20);
-    rect(this.location.x - 40, this.location.y - 100, 20, 20);
-    rect(this.location.x - 20, this.location.y - 100, 20, 20);
-    rect(this.location.x, this.location.y - 100, 20, 20);
-    rect(this.location.x + 20, this.location.y - 100, 20, 20);
-    rect(this.location.x + 40, this.location.y - 100, 20, 20);
-    rect(this.location.x + 60, this.location.y - 100, 20, 20);
-    rect(this.location.x + 80, this.location.y - 100, 20, 20);
+    rect(this.location.x - this.blockSize*4, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*3, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*3, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*4, this.location.y - this.blockSize*5, this.blockSize, this.blockSize);
 
     //row7
     if (7 <= this.row) {
@@ -255,15 +299,15 @@ display() {
     } else {
       fill(255);
     }
-    rect(this.location.x - 80, this.location.y - 120, 20, 20);
-    rect(this.location.x - 60, this.location.y - 120, 20, 20);
-    rect(this.location.x - 40, this.location.y - 120, 20, 20);
-    rect(this.location.x - 20, this.location.y - 120, 20, 20);
-    rect(this.location.x, this.location.y - 120, 20, 20);
-    rect(this.location.x + 20, this.location.y - 120, 20, 20);
-    rect(this.location.x + 40, this.location.y - 120, 20, 20);
-    rect(this.location.x + 60, this.location.y - 120, 20, 20);
-    rect(this.location.x + 80, this.location.y - 120, 20, 20);
+    rect(this.location.x - this.blockSize*4, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*3, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*3, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*4, this.location.y - this.blockSize*6, this.blockSize, this.blockSize);
 
     //row8
     if (8 <= this.row) {
@@ -272,12 +316,12 @@ display() {
       fill(255);
     }
 
-    rect(this.location.x - 60, this.location.y - 140, 20, 20);
-    rect(this.location.x - 40, this.location.y - 140, 20, 20);
-    rect(this.location.x - 20, this.location.y - 140, 20, 20);
-    rect(this.location.x + 20, this.location.y - 140, 20, 20);
-    rect(this.location.x + 40, this.location.y - 140, 20, 20);
-    rect(this.location.x + 60, this.location.y - 140, 20, 20);
+    rect(this.location.x - this.blockSize*3, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize*2, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
+    rect(this.location.x - this.blockSize, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*2, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
+    rect(this.location.x + this.blockSize*3, this.location.y - this.blockSize*7, this.blockSize, this.blockSize);
   }
 
 
